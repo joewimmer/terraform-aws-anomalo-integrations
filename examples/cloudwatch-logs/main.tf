b642ebdbdc1afd6783279929b8cbf36db6ea34a9
joewimmer <joe@anomalo.com>
2025-07-06 22:58:42 -0400

    update example

--- examples/cloudwatch-logs/main.tf
+++ examples/cloudwatch-logs/main.tf
@@ -2,8 +2,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
+module "anomalo_eventbridge" {
+  source              = "github.com/joewimmer/terraform-aws-anomalo-eventbridge"
+  ip_allow_list       = ["44.230.200.13/32", "104.14.162.108/32"]
+}
 
 module "cloudwatch_logs" {
   source        = "./modules/cloudwatch-logs"
-  event_bus_arn = "arn:aws:events:us-west-2:1234567890:event-bus/anomalo"
+  event_bus_arn = module.anomalo_eventbridge.event_bus_arn
 }
